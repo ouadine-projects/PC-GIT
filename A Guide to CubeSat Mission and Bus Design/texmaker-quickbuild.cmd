8556eba656bc341b78d1cb4d0ce64c6fc742f870
youssef <youssef_taha1@hotmail.com>
2025-08-08 23:06:53 +0100

    tt

--- A Guide to CubeSat Mission and Bus Design/texmaker-quickbuild.cmd
+++
@@ -1,1 +0,0 @@
-pdflatex -synctex=1 -interaction=nonstopmode %.tex|makeindex %.idx -s styles/index.ist|bibtex %.aux|makeglossaries %|pdflatex -synctex=1 -interaction=nonstopmode %.tex|pdflatex -synctex=1 -interaction=nonstopmode %.tex
